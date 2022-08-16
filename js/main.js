1c314fca78077c233c77a08cbd8de970af93bcdb
Gowthaman Ravindrathas <gowtham4n@gmail.com>
2022-08-16 15:36:13 +0100

    adding about me skills

--- js/main.js
+++ js/main.js
@@ -118,3 +118,46 @@ $( '.nav__list-item a' ).on( 'click', function () {
 	$( '.nav__list' ).find( '.nav__list-item a.active-nav-btn' ).removeClass( 'active-nav-btn' );
 	$( this ).addClass( 'active-nav-btn' );
 });
+
+// Vertical tab
+// tabbed content
+// http://www.entheosweb.com/tutorials/css/tabs.asp
+$(".tab_content").hide();
+$(".tab_content:first").show();
+
+/* if in tab mode */
+$("ul.tabs li").click(function () {
+  $(".tab_content").hide();
+  var activeTab = $(this).attr("rel");
+  $("#" + activeTab).fadeIn();
+
+  $("ul.tabs li").removeClass("active");
+  $(this).addClass("active");
+
+  $(".tab_drawer_heading").removeClass("d_active");
+  $(".tab_drawer_heading[rel^='" + activeTab + "']").addClass("d_active");
+
+  /*$(".tabs").css("margin-top", function(){ 
+       return ($(".tab_container").outerHeight() - $(".tabs").outerHeight() ) / 2;
+    });*/
+});
+$(".tab_container").css("min-height", function () {
+  return $(".tabs").outerHeight() + 50;
+});
+/* if in drawer mode */
+$(".tab_drawer_heading").click(function () {
+  $(".tab_content").hide();
+  var d_activeTab = $(this).attr("rel");
+  $("#" + d_activeTab).fadeIn();
+
+  $(".tab_drawer_heading").removeClass("d_active");
+  $(this).addClass("d_active");
+
+  $("ul.tabs li").removeClass("active");
+  $("ul.tabs li[rel^='" + d_activeTab + "']").addClass("active");
+});
+
+/* Extra class "tab_last" 
+	   to add border to bottom side
+	   of last tab 
+	$('ul.tabs li').last().addClass("tab_last");*/
