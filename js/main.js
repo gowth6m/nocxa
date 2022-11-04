d2b5fe0fd7a2001487dcb55185c8e52e3bcba594
Gowthaman Ravindrathas <gowtham4n@gmail.com>
2022-11-04 18:14:11 +0000

    added some animations

--- js/main.js
+++ js/main.js
@@ -160,3 +160,24 @@ $(".tab_drawer_heading").click(function () {
   $(allTabs).removeClass("active-h");
   $(this).addClass("active-h");
 });
+
+$(document).ready(function () {
+  $(window).scroll(function () {
+
+    var wScroll = $(window).scrollTop();
+
+    $('#about-bg-text').css('transform', 'translateX(-' + (wScroll * 0.25) + 'px')
+  });
+
+  /* Check the location of each desired element */
+  $('.hideme').each(function (i) {
+    var bottom_of_object = $(this).offset().top;
+    var bottom_of_window = $(window).scrollTop() + $(window).height();
+
+    /* If the object is completely visible in the window, fade it it */
+    if (bottom_of_window > bottom_of_object) {
+      $(this).animate({ 'opacity': '1' }, 500);
+    }
+  });
+});
+
